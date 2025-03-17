f0913a11844618f138d929a2bc2dc0d8e4c973bd
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-03-17 20:02:18 -0300

    feat: config de cache feita com log, agora começar endpoints

--- src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -3,22 +3,29 @@ package dev.java.ecommerce.basketservice.service;
 import dev.java.ecommerce.basketservice.client.PlatziStoreClient;
 import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final PlatziStoreClient platziStoreClient;
 
+    @Cacheable(value = "Products")
     public List<PlatziProductResponse> getAllProducts(){
+        log.info("Getting all products");
         return platziStoreClient.getAllProducts();
-
     }
 
-    public PlatziProductResponse getProductsById(Long id){
-        return platziStoreClient.getProductById(id);
+    @Cacheable(value = "product", key = "#productid")
+    public PlatziProductResponse getProductsById(Long productId){
+        log.info("Getting product with id: {}", productId);
+        return platziStoreClient.getProductById(productId);
     }
+
 }

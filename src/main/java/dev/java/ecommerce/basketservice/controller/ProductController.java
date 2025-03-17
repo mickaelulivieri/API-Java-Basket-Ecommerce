7455df46aa50a97361134d36011fc27b4dc38bc0
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-03-17 19:19:34 -0300

    feat: primeiro metodo desenvolvido

--- src/main/java/dev/java/ecommerce/basketservice/controller/ProductController.java
+++ src/main/java/dev/java/ecommerce/basketservice/controller/ProductController.java
@@ -1,4 +1,28 @@
 package dev.java.ecommerce.basketservice.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<Void> getAllProducts(){
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Void> getProductById(@PathVariable Long id){
+
+        return ResponseEntity.ok().build();
+    }
 }
---
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -0,0 +1,15 @@
+package dev.java.ecommerce.basketservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    public void getAllProducts(){
+
+    }
+
+    public void getProductsById(Long id){
+
+    }
+}

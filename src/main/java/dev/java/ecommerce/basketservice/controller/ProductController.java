24914645b4243b3ab851d070795c9f17956e1031
mickaelulivieri <mmickael.carvalho@gmail.com>
2025-03-17 19:39:18 -0300

    feat: desenvolvida a conexao com a API e seus metodos

--- src/main/java/dev/java/ecommerce/basketservice/controller/ProductController.java
+++ src/main/java/dev/java/ecommerce/basketservice/controller/ProductController.java
@@ -1,11 +1,15 @@
 package dev.java.ecommerce.basketservice.controller;
 
+import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
+import dev.java.ecommerce.basketservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
@@ -15,14 +19,13 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<Void> getAllProducts(){
-
-        return ResponseEntity.ok().build();
+    public ResponseEntity<List<PlatziProductResponse>> getAllProducts(){
+        return ResponseEntity.ok(productService.getAllProducts());
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<Void> getProductById(@PathVariable Long id){
+    public ResponseEntity<PlatziProductResponse> getProductById(@PathVariable Long id){
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(productService.getProductsById(id));
     }
 }
--- src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
+++ src/main/java/dev/java/ecommerce/basketservice/service/ProductService.java
@@ -1,15 +1,24 @@
 package dev.java.ecommerce.basketservice.service;
 
+import dev.java.ecommerce.basketservice.client.PlatziStoreClient;
+import dev.java.ecommerce.basketservice.client.response.PlatziProductResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
 
-    public void getAllProducts(){
+    private final PlatziStoreClient platziStoreClient;
+
+    public List<PlatziProductResponse> getAllProducts(){
+        return platziStoreClient.getAllProducts();
 
     }
 
-    public void getProductsById(Long id){
-
+    public PlatziProductResponse getProductsById(Long id){
+        return platziStoreClient.getProductById(id);
     }
 }
